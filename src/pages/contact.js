a4bbb712fb169c24fce9567a4dcc4c96f6d0f8f6
squarepancakes <goh.shuyun@gmail.com>
2021-02-20 19:27:02 +0800

    contact us  form and redirection

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,16 +12,43 @@ export default function Contact() {
       </div>
 
       <div>
-        <form name="Contact Form" method="POST" data-netlify="true">
+        <form
+          name="Contact Form"
+          method="POST"
+          data-netlify="true"
+          action="/thank-you"
+        >
           <input type="hidden" name="form-name" value="Contact Form" />
+
           <div>
-            <label>Your Email:</label>
-            <input type="email" name="email" />
+            <label htmlFor="name">
+              Name:
+              <input name="name" />
+            </label>
           </div>
           <div>
-            <label>Message:</label>
-            <textarea name="message" />
+            <label htmlFor="email">
+              Your Email:
+              <input type="email" name="email" />
+            </label>
           </div>
+          <div>
+            <label htmlFor="enquiryType">
+              Type of Enquiry:
+              <select name="enquiry type">
+                <option value="General Enquiry">General Enquiry</option>
+                <option value="Renovation">Renovation</option>
+                <option value="Partnerships">Partnerships</option>
+              </select>
+            </label>
+          </div>
+          <div>
+            <label htmlFor="message">
+              Message:
+              <textarea name="message" />
+            </label>
+          </div>
+
           <button type="submit">Send</button>
         </form>
       </div>
---
+++ src/pages/thank-you.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const ThankYou = () => (
+  <Layout>
+    <h1>Contact</h1>
+    <p>
+      Thank you for reaching out, our team will get back to you within 3 - 5
+      working days!
+    </p>
+  </Layout>
+)
+
+export default ThankYou
